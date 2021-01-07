e54b47592cf29a7d8dca080afb42e7be7dbb4b1a
brunoBMonteiro <brunobilheri@hotmail.com>
2021-01-07 09:45:50 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
-let content = canvas.getContext("2d");
+let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
-    content.fillStyle = "lightgreen";
-    content.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
 criarBG();
+criarCobrinha();
